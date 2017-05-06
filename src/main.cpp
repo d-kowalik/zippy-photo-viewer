da5da9530431b1ec3daa3306266b731a4ba99d7f
Damian Kowalik <dev.aeonal@gmail.com>
2017-05-06 13:31:11 +0200

    remove even more native window attempts

--- src/main.cpp
+++ src/main.cpp
@@ -36,8 +36,6 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    EventFilter eventFilter;
-    app.installNativeEventFilter(&eventFilter);
 
     engine.rootContext()->setContextProperty("archive", archive.data());
     engine.rootContext()->setContextProperty("myModel", &model);
